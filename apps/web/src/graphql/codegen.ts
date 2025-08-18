621d946ef0e506ded0de42548de4592553bd822b
Adam Bartski <adam_bartski@discovery.com>
2025-08-18 16:39:05 +0200

    rm -rf .turbo

--- apps/web/src/graphql/codegen.ts
+++ apps/web/src/graphql/codegen.ts
@@ -3,14 +3,21 @@ import { join } from 'path';
 
 const config: CodegenConfig = {
   schema: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/graphql',
-  documents: join(process.cwd(), '../../packages/contracts/graphql/operations/**/*.graphql'),
+  documents: join(
+    process.cwd(),
+    '../../packages/contracts/graphql/operations/**/*.graphql'
+  ),
   generates: {
     './src/graphql/__generated__/': {
       preset: 'client',
       plugins: [],
     },
     './src/graphql/__generated__/react-query.ts': {
-      plugins: ['typescript', 'typescript-operations', 'typescript-react-query'],
+      plugins: [
+        'typescript',
+        'typescript-operations',
+        'typescript-react-query',
+      ],
       config: {
         fetcher: {
           func: '../client#gqlClient.request',
@@ -19,6 +26,7 @@ const config: CodegenConfig = {
         exposeFetcher: true,
         exposeQueryKeys: true,
         addSuspenseQuery: true,
+        reactQueryVersion: 5,
       },
     },
   },
